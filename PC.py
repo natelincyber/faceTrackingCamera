51955a51c66b261d9042b377dedac68ba24dae99
natelincyber <natelin200510@gmail.com>
2021-06-02 22:03:39 -0700

    modified:   PC.py 	modified:   raspberry.py

--- PC.py
+++ PC.py
@@ -1,17 +1,17 @@
-import socket, os, sys, cv2
+import socket, os, sys, cv2, time
 
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-global s
+HOST = '192.168.0.109'  # The server's hostname or IP address
+PORT = 12345            # The port used by the server
+
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
 # connect to remote camera and get live video stream
 cameraIPAddress = '192.168.0.180:8080'
+
+
 print('connecting to remote camera...')
-
-
 capturePort = cv2.VideoCapture(f'http://{cameraIPAddress}/video')
 print(f"connected to: {cameraIPAddress}")
 
@@ -20,8 +20,15 @@ print(f"connected to: {cameraIPAddress}")
 faceCascade = cv2.CascadeClassifier('faceData/haarcascade_frontalface_default.xml')
 
 # connect to raspberry pi for remote data transfer
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+try:
+    print('connecting to raspberry pi remote server...')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
+    print('connected to remote server') 
+except socket.error:
+    input('unable to connect to remote server. press enter to exit')
+    sys.exit()
+
 
 while(True):
     ret, frame = capturePort.read()
@@ -30,7 +37,8 @@ while(True):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(20,20))
     except cv2.error as e:
-        input("connection lost! press enter to exit")
+        s.sendall(str.encode('close'))
+        input("connection to ipcam lost! press enter to exit")
         sys.exit()
 
     for (x, y, w, h) in faces:
@@ -39,17 +47,18 @@ while(True):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
+
     
     '''
     TODO: code out servo calculations
     and sending calculations over to RPi
-    if inp == 'stop':
-        break
-    s.sendall(str.encode(inp))
     '''
+    s.sendall(str.encode(input()))
+
     cv2.imshow('video', frame)
 
     if cv2.waitKey(1)  == 27:
+        s.sendall(str.encode('close'))
         break
 
 capturePort.release()
--- raspberry.py
+++ raspberry.py
@@ -1,20 +1,50 @@
 import socket
+from servoControl import servoController
 
-HOST = '127.0.0.1' 
-PORT = 65432       
+HOST = '192.168.0.109'
+PORT = 12345       
 
 ''' TODO: recieve data from PC and use servocontroller to move servos'''
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+
+def server(conn,addr):
+    '''
+    runner = servoController(17, 'someServo')
+    runner.calibrate()'''
     with conn:
-        print('Connected by', addr)
+        print('Connected to', addr)
         while True:
+            
             data = conn.recv(2048).decode()
-            if not data:
+            
+            if not data: # prevents null values from being printed on screen
+                continue
+                
+            if data == 'close':
+                print('closing remote connection')
+                conn.close()
                 break
+                
+            if data == 'stop':
+                print('closing remote connection')
+                conn.close()
+                return True
+
             print(data)
-            # for debugging?
-            conn.send(str.encode(data))
+            #runner.move(int(data))
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+print("server started, waiting for connections")
+
+
+while True:
+    conn, addr = s.accept()
+    stop = server(conn,addr)
+    if stop:
+        print('stopping server')
+        break 
+
+
